57a8a5b6aba27e8439c23766458bb91628e6ce5a
KubiakJakub01 <kubiak.jakub01@gmail.com>
2022-12-15 18:26:37 +0100

    Import necessary libraries

--- src/train.py
+++ src/train.py
@@ -1,3 +1,20 @@
 """
 Main training script for the project.
 """
+
+# Import basic libraries
+import argparse
+from datetime import datetime
+from datasets import load_dataset
+
+# Load the DistilBERT tokenizer to process the text field:
+from transformers import (
+    DataCollatorWithPadding,
+    create_optimizer,
+)
+
+# Import metrics
+from transformers.keras_callbacks import KerasMetricCallback, PushToHubCallback
+from keras.callbacks import TensorBoard
+
+
