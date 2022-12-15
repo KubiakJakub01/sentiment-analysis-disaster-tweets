bec05d60f208579ff4efe969da22b65c75ae0dfd
KubiakJakub01 <kubiak.jakub01@gmail.com>
2022-12-15 19:36:40 +0100

    Load args from json file

--- src/train.py
+++ src/train.py
@@ -4,6 +4,9 @@ Main training script for the project.
 
 # Import basic libraries
 import argparse
+import os
+import sys
+import json
 from datetime import datetime
 from datasets import load_dataset
 
@@ -17,63 +20,23 @@ from transformers import (
 from transformers.keras_callbacks import KerasMetricCallback, PushToHubCallback
 from keras.callbacks import TensorBoard
 
-def get_params():
-    """Get the parameters for the training.
+# Import modules from src
+from src.utils.params import train_params
 
-    Returns:
-        args (argparse.Namespace): Namespace containing the parameters."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--train_path",
-        "-t",
-        type=str,
-        default="data/train.csv",
-        help="Path to the train set.",
-    )
-    parser.add_argument(
-        "--valid_path",
-        "-v",
-        type=str,
-        default="data/valid.csv",
-        help="Path to the valid set.",
-    )
-    parser.add_argument(
-        "--model_name",
-        "-m",
-        type=str,
-        default="distilbert-base-uncased",
-        help="Name of the model to use.",
-    )
-    parser.add_argument(
-        "--output_dir",
-        "-o",
-        type=str,
-        default="output",
-        help="Output directory for the model.",
-    )
-    parser.add_argument(
-        "--augumented_path",
-        "-a",
-        type=str,
-        default="data/augumented.csv",
-        help="Path to the augumented set.",
-        required=False,
-    )
-    parser.add_argument(
-        "--num_labels",
-        type=int,
-        default=2,
-        help="Number of labels to use.",
-        required=False,
-    )
-    parser.add_argument(
-        "--target_label",
-        type=str,
-        default="label",
-        help="Target label to use.",
-        required=False,
-    )
-    args = parser.parse_args()
-    return args
-
-
+if __name__ == "__main__":
+    
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+        # Load the parameters from the config file
+        with open(sys.argv[1]) as f:
+            params = train_params(**json.load(f))
+    else:
+        print("""No config file provided. Specify a config file with the following format:
+                { "train_path": "path/to/train.csv",
+                  "valid_path": "path/to/valid.csv",
+                  "model_name": "distilbert-base-uncased",
+                  "output_dir": "path/to/output_dir",
+                  "num_labels": 2,
+                  "target_label": "label",
+                  "augmented_path": "path/to/augmented.csv""")
+        sys.exit(1)
+    print(params)
