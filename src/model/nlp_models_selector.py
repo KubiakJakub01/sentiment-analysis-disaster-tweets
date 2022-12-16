1bf4906b322ecab6ad570253bff663e1a765ee52
KubiakJakub01 <kubiak.jakub01@gmail.com>
2022-12-16 17:04:40 +0100

    Fix args list in get_model_and_tokenizer function

--- src/model/nlp_models_selector.py
+++ src/model/nlp_models_selector.py
@@ -5,29 +5,34 @@ within this class.
 """
 #pylint: disable=import-outside-topleve
 
-def get_model_and_tokenizer(self) -> object:
+def get_model_and_tokenizer(model_name: str, num_label: int) -> object:
     """Selects the model type based on the model_name and num_label.
+
+    Args:
+        model_name (str): Name of the model to use.
+        num_label (int): Number of labels to use.
+        
     Returns:
         model (transformers.modeling_tf_utils.TFPreTrainedModel): Model to use for training.
         tokenizer (transformers.tokenization_utils_base.PreTrainedTokenizerBase):
             Tokenizer to use for encoding the data.
     """
-    if "distilbert" in self.model_name:
+    if "distilbert" in model_name:
         from transformers import (
             TFDistilBertModel,
             DistilBertTokenizer,
             DistilBertConfig,
         )
-        config = DistilBertConfig(num_labels=self.num_label)
-        model = TFDistilBertModel.from_pretrained(self.model_name, config=config)
-        tokenizer = DistilBertTokenizer.from_pretrained(self.model_name)
+        config = DistilBertConfig(num_labels=num_label)
+        model = TFDistilBertModel.from_pretrained(model_name, config=config)
+        tokenizer = DistilBertTokenizer.from_pretrained(model_name)
 
-    elif "TFAutoModel" in self.model_name:
+    elif "TFAutoModel" in model_name:
         from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
         model = TFAutoModelForSequenceClassification.from_pretrained(
-            self.model_name, num_labels=self.num_label
+            model_name, num_labels=num_label
         )
-        tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     else:
         raise ValueError("Model not supported.")
