d0bfdab85a8ce601c65b85a777e26b62a6bba130
KubiakJakub01 <kubiak.jakub01@gmail.com>
2022-12-16 16:49:43 +0100

    Add load_dataset_from_csv function

--- src/train.py
+++ src/train.py
@@ -3,7 +3,6 @@ Main training script for the project.
 """
 
 # Import basic libraries
-import argparse
 import os
 import sys
 import json
@@ -23,12 +22,45 @@ from keras.callbacks import TensorBoard
 # Import modules from src
 from src.utils.params import train_params
 
+def load_dataset_from_csv(train_path: str, valid_path: str, augument_path: str) -> None:
+    """Load the train and valid sets.
+
+    Args:
+        train_path (str): Path to the train set.
+        valid_path (str): Path to the valid set.
+        augument_path (str): Path to the augumented set.
+
+    Returns:
+        dataset (dict): Dictionary containing the train and valid sets."""
+    if augument_path:
+        dataset = load_dataset(
+            "csv",
+            data_files={"train": [train_path, augument_path], "validation": valid_path},
+        )
+    else:
+        dataset = load_dataset(
+            "csv", data_files={"train": train_path, "validation": valid_path}
+        )
+    return dataset
+
+def train(params):
+    """Pipeline for training the model.
+
+    Args:
+        params (dict): Dictionary of parameters.
+    
+    Returns:
+        Saves the trained model to the output directory.
+    """
+    pass
+
+
 if __name__ == "__main__":
     
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
         # Load the parameters from the config file
         with open(sys.argv[1]) as f:
-            params = train_params(**json.load(f))
+            params_ = train_params(**json.load(f))
     else:
         print("""No config file provided. Specify a config file with the following format:
                 { "train_path": "path/to/train.csv",
@@ -39,4 +71,7 @@ if __name__ == "__main__":
                   "target_label": "label",
                   "augmented_path": "path/to/augmented.csv""")
         sys.exit(1)
-    print(params)
+    print(params_)
+
+    # Train the model
+    train(params_)
