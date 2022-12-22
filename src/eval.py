8297c2e5cb6eb8ed6061b95282469dbe5fa86d58
Jakub <kubiak.jakub01@gmail.com>
2022-12-22 21:51:21 +0100

    Add get_params function

--- src/eval.py
+++ src/eval.py
@@ -16,3 +16,43 @@ from transformers import pipeline
 from src.model.nlp_models_selector import get_model_and_tokenizer
 from src.utils.compute_results import get_results, save_results
 from src.utils.nlp_metric import Metric
+
+
+def get_params():
+    """Get parameters from command line.
+    
+    Returns:
+        args (argparse.Namespace): Arguments from command line."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_path",
+        "-m",
+        type=str,
+        help="Path to checkpoint with the model to use.",
+    )
+    parser.add_argument(
+        "--path_to_test_data",
+        "-t",
+        type=str,
+        default="data/test.csv",
+        help="Path to the test set.",
+    )
+    parser.add_argument(
+        "--num_labels",
+        "-n",
+        type=int,
+        default=2,
+        help="Number of labels for the model.",
+    )
+    parser.add_argument(
+        "--save_predictions_path", "-s", type=str, help="Path to save the predictions."
+    )
+    parser.add_argument(
+        "--metrics",
+        "-e",
+        nargs="+",
+        type=str,
+        help="Metrics to use.",
+        default=["accuracy", "precision", "recall", "f1"],
+    )
+    return parser.parse_args()
