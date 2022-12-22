06706d59ea549f60afcdb5567d81a2103c14614a
Jakub <kubiak.jakub01@gmail.com>
2022-12-22 21:41:46 +0100

    Add main run script pipeline

--- src/utils/compute_results.py
+++ src/utils/compute_results.py
@@ -14,7 +14,7 @@ from src.utils.nlp_metric import Metric
 
 def get_params():
     """Get the parameters from command line.
-    
+
     Returns:
         args (argparse.Namespace): Arguments from command line."""
     parser = argparse.ArgumentParser()
@@ -38,23 +38,23 @@ def get_params():
 
 def read_predictions(path_to_predictions):
     """Read predictions from json file.
-    
+
     Args:
         path_to_predictions (str): Path to the predictions.
-    
+
     Returns:
         preds (dict): Dictionary containing the predictions."""
-    with open(path_to_predictions, "r") as f:
+    with open(path_to_predictions, "r", encoding="utf-8") as f:
         preds = json.load(f)
     return preds
 
 
 def read_labels(path_to_labels):
     """Read labels from a csv file.
-    
+
     Args:
         path_to_labels (str): Path to the labels.
-        
+
     Returns:
         labels (list): List containing the labels."""
     df = pd.read_csv(path_to_labels)
@@ -86,5 +86,36 @@ def save_results(results, save_predictions_path):
         results (dict): Dictionary with the results.
         save_predictions_path (str): Path to save the results."""
     if save_predictions_path:
-        with open(save_predictions_path / "metrics.txt", "w") as f:
+        with open(save_predictions_path / "metrics.txt", "w", encoding="utf-8") as f:
             f.write(str(results))
+
+
+if __name__ == "__main__":
+    # Get parameters
+    params = get_params()
+
+    print(f"Path to predictions: {params.path_to_predictions}")
+    # Load predictions
+    preds = read_predictions(Path(params.path_to_predictions))
+
+    print(f"Path to labels: {params.path_to_labels}")
+    # Load labels
+    labels = read_labels(Path(params.path_to_labels))
+
+    print(f"Labels: {labels[0]}")
+    print(f"Predictions: {preds[0]}")
+
+    print(f"Metrices: {params.metrics}")
+    # Load metrics
+    metrics = [Metric(metric) for metric in params.metrics]
+
+    print("Computing results...")
+    # Compute metrics
+    results = get_results(preds, labels, metrics)
+
+    # Print metric results
+    print(results)
+
+    print(f"Path to save results: {params.path_to_save_results}")
+    # Save metric results
+    save_results(results, Path(params.path_to_save_results))
