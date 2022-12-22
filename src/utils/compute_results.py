ab133788e002bf17fb9d6c54070fb9f6e02b8b9f
Jakub <kubiak.jakub01@gmail.com>
2022-12-22 21:35:01 +0100

    Import libs and modules

--- src/utils/compute_results.py
+++ src/utils/compute_results.py
@@ -1,3 +1,12 @@
 """
 Compute results based on the predictions and labels.
 """
+# Imports basic libraries
+import argparse
+import json
+from pathlib import Path
+
+import pandas as pd
+
+# Import modules
+from src.utils.nlp_metric import Metric
