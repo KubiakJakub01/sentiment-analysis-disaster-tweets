110c740493cf7e9e60f746488a2e29f8f653bac7
Jakub <kubiak.jakub01@gmail.com>
2022-12-22 21:35:59 +0100

    Add get_params function

--- src/utils/compute_results.py
+++ src/utils/compute_results.py
@@ -10,3 +10,28 @@ import pandas as pd
 
 # Import modules
 from src.utils.nlp_metric import Metric
+
+
+def get_params():
+    """Get the parameters from command line.
+    
+    Returns:
+        args (argparse.Namespace): Arguments from command line."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path_to_labels", "-l", type=str, help="Path to the labels.")
+    parser.add_argument(
+        "--path_to_predictions", "-p", type=str, help="Path to the predictions."
+    )
+    parser.add_argument(
+        "--path_to_save_results", "-s", type=str, help="Path to save the results."
+    )
+    parser.add_argument(
+        "--metrics",
+        "-m",
+        nargs="+",
+        type=str,
+        help="Metrics to use.",
+        default=["accuracy", "precision", "recall", "f1"],
+    )
+    return parser.parse_args()
+    
