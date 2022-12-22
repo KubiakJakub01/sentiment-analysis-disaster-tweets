e81b9760c0bc899f98d30ea581d52c859cb3cc86
Jakub <kubiak.jakub01@gmail.com>
2022-12-22 21:50:30 +0100

    Import necessary libraries and modules

--- src/eval.py
+++ src/eval.py
@@ -1,3 +1,18 @@
 """
-Inference and evaluation for the model.
+Main inference script for the project.
 """
+
+# Imports basic libraries
+import argparse
+import json
+from datetime import datetime
+from pathlib import Path
+
+# Import huggingface libraries
+from datasets import load_dataset
+from transformers import pipeline
+
+# Import modules
+from src.model.nlp_models_selector import get_model_and_tokenizer
+from src.utils.compute_results import get_results, save_results
+from src.utils.nlp_metric import Metric
