cc42549df0f9969a5191f5c92bb0d8f2c649b2cc
Jakub <kubiak.jakub01@gmail.com>
2022-12-31 22:38:42 +0100

    Create pipeline to get prediction

--- src/utils/get_predictions.py
+++ src/utils/get_predictions.py
@@ -1,9 +1,18 @@
 """
 Make predictions on test data and save to final csv file using a trained model.
+
+Usage:
+    python3 -m src.utils.get_predictions -m <model_path> -t <path_to_test_data> -n <num_labels> -s <save_predictions_path>
 """
 import argparse
+from datetime import datetime
+from pathlib import Path
 
+import pandas as pd
+from tqdm import tqdm
 from transformers import pipeline
+
+from src.model.nlp_models_selector import get_model_and_tokenizer
 
 
 def get_params():
@@ -45,7 +54,7 @@ def map_label_to_integers(label):
     return label
     
 
-def get_prdiction(model, tokenizer, text):
+def get_prdiction(model, tokenizer, id, text):
     """Get predictions for the model.
 
     Args:
@@ -55,15 +64,64 @@ def get_prdiction(model, tokenizer, text):
 
     Returns:
         preds (dict): Dictionary with the predictions."""
+    preds = []
     clasificator = pipeline(task="sentiment-analysis", model=model, tokenizer=tokenizer)
-    preds = clasificator(text)
-
-    preds = [
-        {
-            "score": round(pred["score"], 4),
-            "labels": map_label_to_integers(pred["label"]),
-        }
-        for pred in preds
-    ]
+    for pred in tqdm(clasificator(text, batch_size=8), 
+                     desc="Predictions"):
+        preds.append( 
+            {
+                "id": pred["id"],
+                "labels": map_label_to_integers(pred["label"]),
+            })
 
     return preds
+
+
+def save_predictions(preds, save_predictions_path):
+    """Save predictions to json file.
+
+    Args:
+        preds (dict): Dictionary with the predictions.
+        save_predictions_path (str): Path to save the predictions.
+
+    Returns:
+        Create a file with the predictions."""
+    # Save predictions to csv file with the following columns:
+    # id, labels
+    df = pd.DataFrame(preds)
+    df.to_csv(save_predictions_path, index=False)
+
+
+if __name__ == "__main__":
+    # Get parameters from command line
+    args = get_params()
+
+    # Define constant variables
+    MODEL_PATH = Path(args.model_path)
+    MODEL_NAME = MODEL_PATH.name
+    NUM_LABELS = args.num_labels
+    START_TIME = datetime.now()
+    START_TIME = START_TIME.strftime("%Y-%m-%d_%H-%M-%S")
+    SAVE_PREDICTIONS_PATH = Path(args.save_predictions_path) / MODEL_NAME / START_TIME
+
+    # Load test data
+    df = pd.read_csv(args.path_to_test_data)
+
+    # Get model and tokenizer
+    print("Loading model...")
+    model, tokenizer = get_model_and_tokenizer(model_name=str(MODEL_PATH), 
+                                               num_labels=NUM_LABELS)
+
+    # Get predictions
+    print("Making predictions...")
+    preds = get_prdiction(model, 
+                        tokenizer, 
+                        df["id"].to_list(),
+                        df["text"].tolist())
+
+    # Save predictions
+    print("Saving predictions...")
+    Path(SAVE_PREDICTIONS_PATH).parent.mkdir(parents=True, exist_ok=True)
+    save_predictions(preds, SAVE_PREDICTIONS_PATH)
+
+    print("Done!")
