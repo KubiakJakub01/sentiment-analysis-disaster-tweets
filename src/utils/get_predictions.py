7169e26fe4ecd7f756c3fc2c79660be0634b96a7
Jakub <kubiak.jakub01@gmail.com>
2022-12-31 16:39:48 +0100

    Add get_params function

--- src/utils/get_predictions.py
+++ src/utils/get_predictions.py
@@ -1,8 +1,43 @@
 """
 Make predictions on test data and save to final csv file using a trained model.
 """
+import argparse
+
 from transformers import pipeline
 
+
+def get_params():
+    """Get parameters from command line.
+
+    Returns:
+        args (argparse.Namespace): Arguments from command line."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_path",
+        "-m",
+        type=str,
+        help="Path to checkpoint with the model to use.",
+    )
+    parser.add_argument(
+        "--path_to_test_data",
+        "-t",
+        type=str,
+        default="data/test.csv",
+        help="Path to the test set.",
+    )
+    parser.add_argument(
+        "--num_labels",
+        "-n",
+        type=int,
+        default=2,
+        help="Number of labels for the model.",
+    )
+    parser.add_argument(
+        "--save_predictions_path", 
+        "-s", type=str, 
+        help="Path to save the predictions."
+    )
+    return parser.parse_args()
 
 def map_label_to_integers(label):
     """Map labels to integers."""
