67b425739157d9d73d0382ee4d15a30c6ce33d03
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-01-13 16:06:38 +0100

    Parse new model_params class

--- src/utils/params.py
+++ src/utils/params.py
@@ -12,24 +12,40 @@ from typing import List, Optional
 class TrainParams:
     train_path: str = field(metadata={"help": "Path to the train set."})
     valid_path: str = field(metadata={"help": "Path to the valid set."})
-    output_dir: str = field(metadata={"help": "Output directory for the training artifacts."})
+    output_dir: str = field(
+        metadata={"help": "Output directory for the training artifacts."}
+    )
     target_label: str = field(metadata={"help": "Target label to use."})
     text_column: str = field(metadata={"help": "Column containing the text to use."})
-    remove_columns: Optional[List[str]] = field(default_factory=list, metadata={"help": "Columns to remove from the dataset."})
-    augmented_path: Optional[str] = field(default=None, metadata={"help": "Path to the augmented set."})
+    remove_columns: Optional[List[str]] = field(
+        default_factory=list, metadata={"help": "Columns to remove from the dataset."}
+    )
+    augmented_path: Optional[str] = field(
+        default=None, metadata={"help": "Path to the augmented set."}
+    )
+
 
 @dataclass
 class ModelParams:
     model_name: str = field(metadata={"help": "Name of the model to use."})
     model_output_dir: str = field(metadata={"help": "Output directory for the model."})
-    model_save_name: Optional[str] = field(default=None, metadata={"help": "Name of the model to save."})
-    hub_model_id: Optional[str] = field(default=None, metadata={"help:": "Your huggingface-hub username. Default: None"})
-    add_layers: Optional[bool] = field(default=False, metadata={"help": "If add input and binary output layers."})
-    num_labels: Optional[int] = field(default=2, metadata={"help": "Number of labels to use. Default: 2"})
+    model_save_name: Optional[str] = field(
+        default=None, metadata={"help": "Name of the model to save."}
+    )
+    hub_model_id: Optional[str] = field(
+        default=None, metadata={"help:": "Your huggingface-hub username. Default: None"}
+    )
+    add_layers: Optional[bool] = field(
+        default=False, metadata={"help": "If add input and binary output layers."}
+    )
+    num_labels: Optional[int] = field(
+        default=2, metadata={"help": "Number of labels to use. Default: 2"}
+    )
 
     def __post_init__(self):
         if self.model_save_name is None:
             self.model_save_name = self.model_name
+
 
 @dataclass
 class Hyperparameters:
@@ -42,16 +58,23 @@ class Hyperparameters:
     batch_size: int = field(metadata={"help": "Batch size for training."})
     weight_decay: float = field(metadata={"help": "Weight decay to use."})
     warmup_steps: int = field(metadata={"help": "Number of warmup steps."})
-    save_strategy: str = field(metadata={"help": "Strategy to use for saving the model."})
+    save_strategy: str = field(
+        metadata={"help": "Strategy to use for saving the model."}
+    )
     evaluation_strategy: str = field(metadata={"help": "Evaluation strategy to use."})
     eval_steps: int = field(metadata={"help": "Number of steps to evaluate the model."})
-    load_best_model_at_end: bool = field(metadata={"help": "Load the best model at the end."})
-    greater_is_better: bool = field(metadata={"help": "Whether the metric is greater is better."})
+    load_best_model_at_end: bool = field(
+        metadata={"help": "Load the best model at the end."}
+    )
+    greater_is_better: bool = field(
+        metadata={"help": "Whether the metric is greater is better."}
+    )
 
 
 @dataclass
 class Params:
     train_params: TrainParams
+    model_params: ModelParams
     hyperparameters: Hyperparameters
 
 
@@ -68,13 +91,13 @@ def get_params(json_file_path) -> Params:
         with open(json_file_path, "r") as f:
             json_loader = json.load(f)
         train_params = TrainParams(**json_loader["train_params"])
+        model_params = ModelParams(**json_loader["model_params"])
         hyperparameters = Hyperparameters(**json_loader["hyperparameters"])
-        params = Params(train_params=train_params, hyperparameters=hyperparameters)
+        params = Params(
+            train_params=train_params,
+            model_params=model_params,
+            hyperparameters=hyperparameters,
+        )
     else:
         raise FileNotFoundError(f"File {json_file_path} not found.")
     return params
-
-if __name__ == "__main__":
-    params = get_params("src/config/params.json")
-    print(params.train_params.train_path)
-    print(params.hyperparameters.learning_rate)
